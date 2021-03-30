ea2b374d2c96f36b2a8dcb652a527b8f5dd6028f
KwnstantinosSt <kwnstantinosst@outlook.com>
2021-03-30 14:48:43 +0300

    Improve loading movies

--- visual studio/IMBD-adopse/HomeForm.cs
+++ visual studio/IMBD-adopse/HomeForm.cs
@@ -10,55 +10,94 @@ namespace IMBD_adopse
         public HomeForm()
         {
             InitializeComponent();
+            loadHome();
+        }
+
+        //load home movies
+        public void loadHome()
+        {
+            //stougiannou Endeiktikh Prosegisi
+            Movie obj = new Movie();
+            List<Movie> movies = obj.getTopMovies();
+            foreach (Movie movie in movies)
+            {
+                PictureBox movieImage = new PictureBox();
+                movieImage.ImageLocation = movie.Photo;
+                movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                movieImage.Click += (sender, e) =>
+                {
+                    GlimpseForm glimpseForm = new GlimpseForm(movie.Id);
+                    glimpseForm.Show();
+                };
+                flowPanelTopMovies.Controls.Add(movieImage);
+            }
+
+            Movie obj2= new Movie();
+            List<Movie> movies2 = obj.getMovies(10,"desc");
+            foreach (Movie movie2 in movies2)
+            {
+                PictureBox movieImage = new PictureBox();
+                movieImage.ImageLocation = movie2.Photo;
+                movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
+                movieImage.Click += (sender, e) =>
+                {
+                    GlimpseForm glimpseForm = new GlimpseForm(movie2.Id);
+                    glimpseForm.Show();
+                };
+                flowPanelNewReleases.Controls.Add(movieImage);
+            }
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             //Fill New Releases with random movies
-            for (int i = 0; i < 6; i++)
-            {
-                //Get DB data
-                Movie obj = new Movie();
-                List<Movie> movies = obj.getMovies(11 + i);
+            /* for (int i = 0; i < 6; i++)
+             {
+                 //Get DB data
+                 Movie obj = new Movie();
+                 List<Movie> movies = obj.getMovies(11 + i);
 
-                //Create Picture Box
-                PictureBox movieImage = new PictureBox();
-                movieImage.ImageLocation = movies[0].Photo;
-                movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
-                
-                //Add Click Event
-                movieImage.Click += (sender, e) =>
-                {
-                    GlimpseForm glimpseForm = new GlimpseForm(movies[0].Id);
-                    glimpseForm.Show();
-                };
+                 //Create Picture Box
+                 PictureBox movieImage = new PictureBox();
+                 movieImage.ImageLocation = movies[0].Photo;
+                 movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
 
-                //Assign to Flow Panel
-                flowPanelNewReleases.Controls.Add(movieImage);
-            }
+                 //Add Click Event
+                 movieImage.Click += (sender, e) =>
+                 {
+                     GlimpseForm glimpseForm = new GlimpseForm(movies[0].Id);
+                     glimpseForm.Show();
+                 };
+
+                 //Assign to Flow Panel
+                 flowPanelNewReleases.Controls.Add(movieImage);
+             }*/
 
             //Fill Top Movies with random movies
-            for (int i = 0; i < 6; i++)
-            {
-                //Get DB data
-                Movie obj = new Movie();
-                List<Movie> movies = obj.getMovies(21 + i);
+            /*  for (int i = 0; i < 6; i++)
+              {
+                  //Get DB data
+                  Movie obj = new Movie();
+                  List<Movie> movies = obj.getMovies(21 + i);
 
-                //Create Picture Box
-                PictureBox movieImage = new PictureBox();
-                movieImage.ImageLocation = movies[0].Photo;
-                movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
+                  //Create Picture Box
+                  PictureBox movieImage = new PictureBox();
+                  movieImage.ImageLocation = movies[0].Photo;
+                  movieImage.SizeMode = PictureBoxSizeMode.AutoSize;
 
-                //Add Click Event
-                movieImage.Click += (sender, e) =>
-                {
-                    GlimpseForm glimpseForm = new GlimpseForm(movies[0].Id);
-                    glimpseForm.Show();
-                };
+                  //Add Click Event
+                  movieImage.Click += (sender, e) =>
+                  {
+                      GlimpseForm glimpseForm = new GlimpseForm(movies[0].Id);
+                      glimpseForm.Show();
+                  };
 
-                //Assign to flow panel
-                flowPanelTopMovies.Controls.Add(movieImage);
-            }
+                  //Assign to flow panel
+                  flowPanelTopMovies.Controls.Add(movieImage);
+              }*/
 
         }
     }
