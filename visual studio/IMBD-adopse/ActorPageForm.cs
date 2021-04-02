dcda501e719fd2a837da74505ab194d68dbbfb66
Dimitris Chatzipourganis <dchatzip@yahoo.gr>
2021-04-02 15:16:31 +0300

    Add empty Actor Page

---
+++ visual studio/IMBD-adopse/ActorPageForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMBD_adopse
+{
+    public partial class ActorPageForm : Form
+    {
+        public ActorPageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
