bb05d3c2b991a5346349cea0316c23b56ad45160
Dimitris Chatzipourganis <dchatzip@yahoo.gr>
2021-04-05 17:45:21 +0300

    Background color for app

--- visual studio/IMBD-adopse/ActorPageForm.Designer.cs
+++ visual studio/IMBD-adopse/ActorPageForm.Designer.cs
@@ -29,25 +29,14 @@ namespace IMBD_adopse
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(641, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // ActorPageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackColor = System.Drawing.Color.FloralWhite;
             this.ClientSize = new System.Drawing.Size(1131, 617);
-            this.Controls.Add(this.button1);
             this.Name = "ActorPageForm";
             this.Text = "Actor Page";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -56,7 +45,5 @@ namespace IMBD_adopse
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
