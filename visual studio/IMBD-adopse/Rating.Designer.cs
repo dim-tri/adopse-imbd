0b1dd3d07914765e6bba8279e1dc38939c523ab8
KwnstantinosSt <kwnstantinosst@outlook.com>
2021-04-15 23:05:20 +0300

    Add rating

---
+++ visual studio/IMBD-adopse/Rating.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace IMBD_adopse
+{
+    partial class Rating
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rating));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Rating:";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(167, 3);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.pictureBox5.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5.MouseEnter += new System.EventHandler(this.pictureBox5_MouseEnter);
+            this.pictureBox5.MouseLeave += new System.EventHandler(this.pictureBox5_MouseLeave);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(198, 3);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.pictureBox4.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.pictureBox4.MouseEnter += new System.EventHandler(this.pictureBox4_MouseEnter);
+            this.pictureBox4.MouseLeave += new System.EventHandler(this.pictureBox4_MouseLeave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(104, 3);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.pictureBox3.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_MouseEnter);
+            this.pictureBox3.MouseLeave += new System.EventHandler(this.pictureBox3_MouseLeave);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(135, 3);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.pictureBox2.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.pictureBox1.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // Rating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Rating";
+            this.Size = new System.Drawing.Size(231, 38);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Rating_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ visual studio/IMBD-adopse/Rating.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace IMBD_adopse
+{
+    public partial class Rating : UserControl
+    {
+        public Rating()
+        {
+            InitializeComponent();
+        }
+
+        public bool star1_filled = false;
+        public bool star2_filled = false;
+        public bool star3_filled = false;
+        public bool star4_filled = false;
+        public bool star5_filled = false;
+        public double rating = 0;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+            if(!star2_filled && !star3_filled && !star4_filled && !star5_filled) 
+            {
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px_filled;
+                star1_filled = true;
+            }
+           
+           
+           
+        }
+
+        private void pictureBox3_MouseEnter(object sender, EventArgs e)
+        {
+            if (star1_filled && !star2_filled && !star4_filled && !star5_filled)
+            {
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px_filled;
+                star2_filled = true;
+            }
+           
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            if (star1_filled && star2_filled && !star4_filled && !star5_filled) 
+            {
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px_filled;
+                star3_filled = true;
+            }
+              
+        }
+
+        private void pictureBox5_MouseEnter(object sender, EventArgs e)
+        {
+            if (star1_filled && star3_filled && star2_filled && !star5_filled) 
+            {
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px_filled;
+                star4_filled = true;
+            }
+                
+        }
+
+        private void pictureBox4_MouseEnter(object sender, EventArgs e)
+        {
+            if (star1_filled && star3_filled && star2_filled && star4_filled) 
+            {
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px_filled;
+                star5_filled = true;
+            }
+               
+        }
+
+        private void pictureBox4_MouseLeave(object sender, EventArgs e)
+        {
+           /* if (star1_filled && star2_filled && star3_filled && star4_filled)
+            {
+                pictureBox4.Image = WindowsFormsApp1.Properties.Resources.star_24px;
+                star5_filled = false;
+            }*/
+                
+        }
+
+        private void pictureBox5_MouseLeave(object sender, EventArgs e)
+        {
+          /*  if (star1_filled && star2_filled && star3_filled && !star5_filled)
+            {
+                pictureBox5.Image = WindowsFormsApp1.Properties.Resources.star_24px;
+                star4_filled = false;
+            }*/
+           
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+         /*   if (star1_filled && star2_filled && !star4_filled && !star5_filled)
+            {
+                pictureBox2.Image = WindowsFormsApp1.Properties.Resources.star_24px;
+                star3_filled = false;
+            }*/
+            
+        }
+
+        private void pictureBox3_MouseLeave(object sender, EventArgs e)
+        {
+         /*   if (star1_filled && !star3_filled && !star4_filled && !star5_filled)
+            {
+                pictureBox3.Image = WindowsFormsApp1.Properties.Resources.star_24px;
+                star2_filled = false;
+            }*/
+           
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            //if (!star2_filled && !star3_filled && !star4_filled && !star5_filled)
+          //  {
+              //  pictureBox1.Image = WindowsFormsApp1.Properties.Resources.star_24px;
+            //    star1_filled = false;
+          //  }
+            
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            if (!star2_filled && !star3_filled && !star4_filled && !star5_filled) 
+            {
+                MessageBox.Show("Successfuly Rated with: 2");
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 2;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (star1_filled && !star3_filled && !star4_filled && !star5_filled)
+            {
+               MessageBox.Show("Successfuly Rated with: 4");
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 4;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (star1_filled && star2_filled && !star4_filled && !star5_filled)
+            {
+               MessageBox.Show("Successfuly Rated with: 6");
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 6;
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (star1_filled && star2_filled && star3_filled && !star5_filled)
+            {
+               MessageBox.Show("Successfuly Rated with: 8");
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 8;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (star1_filled && star2_filled && star3_filled && star4_filled)
+            {
+               MessageBox.Show("Successfuly Rated with: 10");
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 10;
+            }
+        }
+
+        public double getRating()
+        {
+            return this.rating;
+        }
+
+        public void setRating(double rate)
+        {
+            this.rating = rate;
+        }
+
+        private void Rating_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                pictureBox1.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox2.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox3.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox4.Image = IMBD_adopse.Properties.Resources.star_24px;
+                pictureBox5.Image = IMBD_adopse.Properties.Resources.star_24px;
+                star1_filled = false;
+                star2_filled = false;
+                star3_filled = false;
+                star4_filled = false;
+                star5_filled = false;
+                rating = 0;
+            }
+        }
+
+    }
+}
+
+
