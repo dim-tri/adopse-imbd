2c86a1400a19f7e3b614841e1e036ec82bd2a054
Dimitris Chatzipourganis <dchatzip@yahoo.gr>
2021-05-10 19:51:03 +0300

    movie page color, position correction

--- visual studio/IMBD-adopse/MoviePage.cs
+++ visual studio/IMBD-adopse/MoviePage.cs
@@ -157,5 +157,15 @@ namespace IMBD_adopse
         {
 
         }
+
+        private void MovieTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
