86bf5cb07f05155c88e68acfca132c3e7fb7ee7e
dim-tri <73897508+dim-tri@users.noreply.github.com>
2021-05-15 21:55:24 +0300

    glimpse fix

--- visual studio/IMBD-adopse/HomeForm.Designer.cs
+++ visual studio/IMBD-adopse/HomeForm.Designer.cs
@@ -41,20 +41,18 @@ namespace IMBD_adopse
             // 
             this.flowPanelNewReleases.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.flowPanelNewReleases.Location = new System.Drawing.Point(35, 89);
-            this.flowPanelNewReleases.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.flowPanelNewReleases.Location = new System.Drawing.Point(31, 67);
             this.flowPanelNewReleases.Name = "flowPanelNewReleases";
-            this.flowPanelNewReleases.Size = new System.Drawing.Size(1479, 391);
+            this.flowPanelNewReleases.Size = new System.Drawing.Size(1294, 293);
             this.flowPanelNewReleases.TabIndex = 5;
             // 
             // flowPanelTopMovies
             // 
             this.flowPanelTopMovies.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.flowPanelTopMovies.Location = new System.Drawing.Point(35, 544);
-            this.flowPanelTopMovies.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.flowPanelTopMovies.Location = new System.Drawing.Point(31, 408);
             this.flowPanelTopMovies.Name = "flowPanelTopMovies";
-            this.flowPanelTopMovies.Size = new System.Drawing.Size(1479, 391);
+            this.flowPanelTopMovies.Size = new System.Drawing.Size(1294, 293);
             this.flowPanelTopMovies.TabIndex = 6;
             // 
             // label1
@@ -64,9 +62,9 @@ namespace IMBD_adopse
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(35, 45);
+            this.label1.Location = new System.Drawing.Point(31, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 37);
+            this.label1.Size = new System.Drawing.Size(145, 30);
             this.label1.TabIndex = 7;
             this.label1.Text = "New Releases";
             // 
@@ -77,9 +75,9 @@ namespace IMBD_adopse
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(34, 500);
+            this.label2.Location = new System.Drawing.Point(30, 375);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(165, 37);
+            this.label2.Size = new System.Drawing.Size(125, 30);
             this.label2.TabIndex = 8;
             this.label2.Text = "Top Movies";
             // 
@@ -90,9 +88,9 @@ namespace IMBD_adopse
             this.recommendedLabel.AutoSize = true;
             this.recommendedLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.recommendedLabel.ForeColor = System.Drawing.Color.White;
-            this.recommendedLabel.Location = new System.Drawing.Point(35, 956);
+            this.recommendedLabel.Location = new System.Drawing.Point(31, 717);
             this.recommendedLabel.Name = "recommendedLabel";
-            this.recommendedLabel.Size = new System.Drawing.Size(308, 37);
+            this.recommendedLabel.Size = new System.Drawing.Size(237, 30);
             this.recommendedLabel.TabIndex = 10;
             this.recommendedLabel.Text = "Recommended for You";
             // 
@@ -100,19 +98,18 @@ namespace IMBD_adopse
             // 
             this.flowPanelRecommended.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.flowPanelRecommended.Location = new System.Drawing.Point(35, 1000);
-            this.flowPanelRecommended.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.flowPanelRecommended.Location = new System.Drawing.Point(31, 750);
             this.flowPanelRecommended.Name = "flowPanelRecommended";
-            this.flowPanelRecommended.Size = new System.Drawing.Size(1479, 391);
+            this.flowPanelRecommended.Size = new System.Drawing.Size(1294, 293);
             this.flowPanelRecommended.TabIndex = 9;
             // 
             // HomeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(123)))), ((int)(((byte)(156)))));
-            this.ClientSize = new System.Drawing.Size(1663, 788);
+            this.ClientSize = new System.Drawing.Size(1523, 591);
             this.ControlBox = false;
             this.Controls.Add(this.recommendedLabel);
             this.Controls.Add(this.flowPanelRecommended);
@@ -121,7 +118,6 @@ namespace IMBD_adopse
             this.Controls.Add(this.flowPanelTopMovies);
             this.Controls.Add(this.flowPanelNewReleases);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "HomeForm";
